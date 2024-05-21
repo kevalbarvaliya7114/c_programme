42320349b9b67b7949104d846c5c9fe34b3d8c66
chetansq <chetan.sq@gmail.com>
2024-05-21 08:44:23 +0530

    21-05-2024

---
+++ areaofcircle.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    float pi=3.14;
+    int r;
+    int A;
+
+    cout << "Enter the number:";
+    cin >> r;
+
+    A=pi*r*r;
+
+    cout << A;
+
+}
